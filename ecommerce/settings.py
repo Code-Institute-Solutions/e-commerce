479da7d96bc2d4da7b8cac14b190eb463a1c6a92
Niel <niel@codeinstitute.net>
2017-10-25 09:06:30 +0000

    add allowed host for Heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'niels-ecommerce.herokuapp.com']
 
 
 # Application definition
