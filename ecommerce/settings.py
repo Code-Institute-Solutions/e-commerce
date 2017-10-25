6b529c1c2c260eb56362a5ae2d0212fbc16629dd
Niel <niel@codeinstitute.net>
2017-10-25 09:30:22 +0000

    change MEDIA_URL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -160,7 +160,7 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
