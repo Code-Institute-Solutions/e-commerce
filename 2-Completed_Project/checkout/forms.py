01a8bc0e99cbb1d5ef98f2e08e29550d22041f94
Yoni Lavi <yoni@codeinstitute.net>
2018-06-29 17:30:38 +0100

    fixed MONTH_CHOICES to include December

--- 2-Completed_Project/checkout/forms.py
+++ 2-Completed_Project/checkout/forms.py
@@ -3,8 +3,8 @@ from .models import Order
 
 class MakePaymentForm(forms.Form):
     
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2017, 2036)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 12+1)]
+    YEAR_CHOICES = [(i, i) for i in range(2018, 2036)]
     
     credit_card_number = forms.CharField(label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
